9ce37ff9fac57f5b9fa373bc7cbfa5bd091fd59c
FabulousBastard <dorian.vidovic1@gmail.com>
2024-02-23 20:19:53 +0100

    sprites2D sample

---
+++ build/main.d
@@ -0,0 +1,61 @@
+main.o: c:/Users/Dorian/Documents/repo/ps3test/source/main.c \
+ c:/PSDK3v2/psl1ght/ppu/include/io/pad.h \
+ c:/PSDK3v2/psl1ght/ppu/include/ppu-types.h \
+ c:/PSDK3v2/psl1ght/ppu/include/sysmodule/sysmodule.h \
+ c:/PSDK3v2/psl1ght/ppu/include/pngdec/pngdec.h \
+ c:/PSDK3v2/ps3dev/portlibs/ppu/include/tiny3d.h \
+ c:/PSDK3v2/psl1ght/ppu/include/sysutil/video.h \
+ c:/PSDK3v2/psl1ght/ppu/include/rsx/rsx.h \
+ c:/PSDK3v2/psl1ght/ppu/include/rsx/mm.h \
+ c:/PSDK3v2/psl1ght/ppu/include/rsx/gcm_sys.h \
+ c:/PSDK3v2/psl1ght/ppu/include/rsx/rsx_program.h \
+ c:/PSDK3v2/psl1ght/ppu/include/rsx/commands.h \
+ c:/PSDK3v2/ps3dev/portlibs/ppu/include/matrix.h \
+ c:/Users/Dorian/Documents/repo/ps3test/build/ghost1_png_bin.h \
+ c:/Users/Dorian/Documents/repo/ps3test/build/ghost2_png_bin.h \
+ c:/Users/Dorian/Documents/repo/ps3test/build/ghost3_png_bin.h \
+ c:/Users/Dorian/Documents/repo/ps3test/build/ghost4_png_bin.h \
+ c:/Users/Dorian/Documents/repo/ps3test/build/ghost5_png_bin.h \
+ c:/Users/Dorian/Documents/repo/ps3test/build/ghost6_png_bin.h \
+ c:/Users/Dorian/Documents/repo/ps3test/build/ghost7_png_bin.h \
+ c:/Users/Dorian/Documents/repo/ps3test/build/ghost8_png_bin.h
+
+c:/PSDK3v2/psl1ght/ppu/include/io/pad.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/ppu-types.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/sysmodule/sysmodule.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/pngdec/pngdec.h:
+
+c:/PSDK3v2/ps3dev/portlibs/ppu/include/tiny3d.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/sysutil/video.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/rsx/rsx.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/rsx/mm.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/rsx/gcm_sys.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/rsx/rsx_program.h:
+
+c:/PSDK3v2/psl1ght/ppu/include/rsx/commands.h:
+
+c:/PSDK3v2/ps3dev/portlibs/ppu/include/matrix.h:
+
+c:/Users/Dorian/Documents/repo/ps3test/build/ghost1_png_bin.h:
+
+c:/Users/Dorian/Documents/repo/ps3test/build/ghost2_png_bin.h:
+
+c:/Users/Dorian/Documents/repo/ps3test/build/ghost3_png_bin.h:
+
+c:/Users/Dorian/Documents/repo/ps3test/build/ghost4_png_bin.h:
+
+c:/Users/Dorian/Documents/repo/ps3test/build/ghost5_png_bin.h:
+
+c:/Users/Dorian/Documents/repo/ps3test/build/ghost6_png_bin.h:
+
+c:/Users/Dorian/Documents/repo/ps3test/build/ghost7_png_bin.h:
+
+c:/Users/Dorian/Documents/repo/ps3test/build/ghost8_png_bin.h:
